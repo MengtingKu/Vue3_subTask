3c76a8482b21c3192f3bdb4c9e81c48f36b29ee4
kuku <polorbear520@gmail.com>
2023-06-10 15:20:45 +0800

    Vue3_compositionAPI:: 點飲料 && 選擇景點

---
+++ src/views/FrontView.vue
@@ -0,0 +1,20 @@
+<template>
+  <header>
+    <ul class="nav nav-tabs">
+      <li class="nav-item">
+        <RouterLink class="nav-link" to="/easy">點飲料(簡易版)</RouterLink>
+      </li>
+      <li class="nav-item">
+        <RouterLink class="nav-link" to="/normal">點飲料(產生訂單)</RouterLink>
+      </li>
+      <li class="nav-item">
+        <RouterLink class="nav-link" to="/viewpoint">景點介紹</RouterLink>
+      </li>
+    </ul>
+  </header>
+  <RouterView />
+</template>
+
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
